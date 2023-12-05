f9591334b6515bb1e098eac69a743eb432969788
Kshitij6000 <94773932+Kshitij6000@users.noreply.github.com>
2023-12-05 20:00:39 +0000

    new changes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,19 @@
+//"use strict"; // treat all JS code as new version 
+
+// alert (3+3) // we are using nodeJS, not browswer 
+
+let name = "Kshitij"
+let age = 20
+let isSingle = true
+
+//number 
+// bigint 
+//string => ""
+//boolean => true/false
+// null => standalone value 
+// undefined =>
+// symbol => unique 
+
+//object 
+
+console.log(typeof null); // output => object 
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const rollNum = 220553;
+let SPI = 8;
+var current_City = "Kanpur"
+fav_song = "One Love"
+let gf_Name ;
+
+// rollNum = 221047 // Can't do this 
+current_City = "Mumbai"
+SPI = 9
+fav_song = "Iraaday"
+
+console.log(rollNum);
+console.table([rollNum,SPI,current_City,fav_song,gf_Name])
