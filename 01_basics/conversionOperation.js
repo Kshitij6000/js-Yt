619182111a21940717d9798e2a39bab347e2a5e2
Kshitij6000 <94773932+Kshitij6000@users.noreply.github.com>
2023-12-05 20:34:46 +0000

    New changes once again

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -9,3 +9,9 @@ let scoreinNumber = Number(score)
 // "33abc" => NaN
 // true => 1 ; false => 0;
 
+//**********Operations**********
+
+console.log("1" + 2);
+console.log(1 +"2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
