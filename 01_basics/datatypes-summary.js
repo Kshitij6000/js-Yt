e747c67be196ff4fcf432455354a57405bdc013d
Kshitij6000 <94773932+Kshitij6000@users.noreply.github.com>
2023-12-07 06:54:46 +0000

    New Commit

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,24 @@
+// Primtive 
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt 
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedin = false
+const outsideTemp = null
+let userEmail; // iski value undefined hogi 
+
+const id = '123'
+const anotherId = '123'
+
+console.log(id === anotherId);
+
+// Refrence / Non - Primitve 
+
+// Arrays, Objects, Functions
+
+const myheroes = ["You", "My BBG", "My Cutie pie"]
+let myObj = {
+    name:"Kshitij",
+    age:20,
+}
